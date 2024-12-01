708c141264d5023d501b509c7e9d495cb1b16e5f
Rafael Violato <rfviolato@gmail.com>
2024-12-01 13:12:48 +0100

    feat: add component for fake content

--- src/app/IOSAppOpenClose/IOSAppOpenClose.tsx
+++ src/app/IOSAppOpenClose/IOSAppOpenClose.tsx
@@ -9,6 +9,7 @@ import {
   useTransform,
 } from "framer-motion";
 import { useRef, useState, type ReactNode } from "react";
+import { AppFakeContent } from "./AppFakeContent";
 
 export interface IOSApp {
   id: string;
@@ -174,23 +175,8 @@ export function IOSAppOpenClose({ apps }: IOSAppOpenCloseProps): ReactNode {
                 <i className={clsx("text-3xl", openedApp.icon)}></i>
               </motion.div>
             </div>
-            <h1 className="text-4xl select-none">App content</h1>
-            <p className="mt-4 select-none">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam
-              voluptatum est ipsum temporibus repellendus officia iusto. Ipsum,
-              dicta! Ea labore harum ut adipisci omnis reiciendis unde neque
-              sint delectus nihil?
-            </p>
-            <p className="mt-2 select-none">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
-              velit, iste quae ex quisquam reprehenderit. Dolore provident hic
-              ratione, eos, sunt dolorum iusto velit earum dolorem corrupti
-              facilis sed tempora. Dolores cum incidunt rerum facilis
-              consequuntur eum consectetur libero distinctio mollitia possimus
-              cupiditate accusamus fuga, labore neque aliquid accusantium optio
-              quasi necessitatibus. Rem totam recusandae nesciunt aperiam
-              suscipit ducimus! Adipisci.
-            </p>
+
+            <AppFakeContent />
 
             <div
               ref={openedAppDragControl}
